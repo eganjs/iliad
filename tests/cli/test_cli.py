dd03b958f881d800a56d7cdf31488b87e7df10d5
Joseph Egan <joseph.s.egan@gmail.com>
2020-08-13 02:15:03 +0100

    Remove dedent in test

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,5 +1,3 @@
-from textwrap import dedent
-
 from click.testing import CliRunner
 
 from iliad.cli import cli
@@ -8,9 +6,6 @@ from iliad.cli import cli
 def test_hello_world():
     runner = CliRunner()
     result = runner.invoke(cli)
+
     assert result.exit_code == 0
-    assert result.output == dedent(
-        """\
-        Hello, World!
-        """
-    )
+    assert result.output == "Hello, World!\n"
