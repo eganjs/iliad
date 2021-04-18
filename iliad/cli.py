7eb85c17efdc9786930993c7e99f7f8f2922c975
Joseph Egan <joseph.s.egan@gmail.com>
2021-04-18 20:11:17 +0100

    Add run command

--- iliad/cli.py
+++ iliad/cli.py
@@ -2,7 +2,12 @@ __all__ = [
     "cli",
 ]
 
-from click import echo, group, version_option
+from io import BufferedReader
+from subprocess import PIPE, Popen
+from typing import IO, AnyStr, Iterator, List, Optional
+
+from click import argument, echo, get_terminal_size, group, secho, style, version_option
+from reprint import output
 
 from iliad.find import find_pyprojects
 
@@ -16,4 +21,83 @@ def cli() -> None:
 @cli.command("list")
 def _list() -> None:
     for pyproject in find_pyprojects():
-        echo("/".join(pyproject.parent.parts))
+        echo("//" + "/".join(pyproject.parent.parts))
+
+
+def io_to_formatted_str(io: IO[AnyStr]) -> Iterator[str]:
+    return (
+        (line.decode() if isinstance(line, bytes) else line).rstrip()
+        for line in io.readlines()
+    )
+
+
+@cli.command("run")
+@argument("args", nargs=-1)
+def _run(args: List[str]) -> None:
+    projects = {
+        "//" + "/".join(pyproject.parent.parts): pyproject
+        for pyproject in find_pyprojects()
+    }
+
+    projects_index = list(enumerate(sorted(projects.keys())))
+
+    failed_processes = dict()
+
+    with output(initial_len=len(projects_index)) as output_lines:
+
+        for idx, project in projects_index:
+            output_lines[
+                idx
+            ] = f"[{style('initializing', fg='bright_black')}] {project}"
+
+        processes = dict()
+
+        for idx, project in projects_index:
+            processes[project] = Popen(
+                ["poetry", "run", *args],
+                cwd=projects[project].parent,
+                stdout=PIPE,
+                stderr=PIPE,
+            )
+            output_lines[idx] = f"[{style('in progress', fg='blue')}] {project}"
+
+        while processes:
+            remaining_processes = dict()
+
+            for idx, project in projects_index:
+                if project not in processes:
+                    continue
+
+                process = processes[project]
+
+                return_code: Optional[int] = process.poll()
+                if return_code is not None:
+
+                    if return_code == 0:
+                        output_lines[idx] = f"[{style('done', fg='green')}] {project}"
+                    else:
+                        output_lines[
+                            idx
+                        ] = f"[{style(f'failed({return_code})', fg='red')}] {project}"
+                        failed_processes[project] = process
+
+                else:
+                    remaining_processes[project] = process
+
+            processes = remaining_processes
+
+    if failed_processes:
+        secho("\nfailures:", bold=True)
+
+        for project, process in failed_processes.items():
+            prefix = f"[{style(project, fg='bright_black')}]"
+
+            echo(f"{prefix} failed with return code {process.returncode}")
+
+            prefix = f"[{style(project + ':stdout', fg='bright_black')}]"
+            for line in io_to_formatted_str(process.stdout):
+                echo(f"{prefix} {style(line, fg='blue')}")
+
+            prefix = f"[{style(project + ':stderr', fg='bright_black')}]"
+            for line in io_to_formatted_str(process.stderr):
+                echo(f"{prefix} {style(line, fg='red')}")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,26 @@
 from pathlib import Path
 from textwrap import dedent
+from time import sleep
+from typing import Iterator
 
+from click import style
 from click.testing import CliRunner
+from pytest import fixture
 
 from iliad.cli import cli
 from iliad.find import find_root
 
 
-def test_version() -> None:
-    runner = CliRunner()
+@fixture
+def runner() -> Iterator[CliRunner]:
+    find_root.cache_clear()
+
+    cli_runner = CliRunner()
+    with cli_runner.isolated_filesystem():
+        yield cli_runner
+
+
+def test_version(runner: CliRunner) -> None:
     result = runner.invoke(cli, ["--version"], catch_exceptions=False)
 
     assert result.exit_code == 0
@@ -46,71 +58,104 @@ def mk_poetry_at(path: Path) -> None:
     )
 
 
-def test_list__success__lists_only_poetry_projects() -> None:
-    find_root.cache_clear()
+def test_list__lists_only_poetry_projects(runner: CliRunner) -> None:
+    (Path.cwd() / ".git").mkdir()
+    mk_poetry_at(Path.cwd() / "alpha")
+    mk_poetry_at(Path.cwd() / "project" / "beta")
+    mk_pyproject_at(Path.cwd() / "project" / "epsilon")
+    mk_poetry_at(Path.cwd() / "project" / "lib" / "delta")
+    mk_pyproject_at(Path.cwd() / "project" / "lib" / "eta")
+    mk_poetry_at(Path.cwd() / "project" / "lib" / "gamma")
+    mk_pyproject_at(Path.cwd() / "project" / "lib" / "zeta")
 
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        (Path.cwd() / ".git").mkdir()
-        mk_poetry_at(Path.cwd() / "alpha")
-        mk_poetry_at(Path.cwd() / "project" / "beta")
-        mk_pyproject_at(Path.cwd() / "project" / "epsilon")
-        mk_poetry_at(Path.cwd() / "project" / "lib" / "delta")
-        mk_pyproject_at(Path.cwd() / "project" / "lib" / "eta")
-        mk_poetry_at(Path.cwd() / "project" / "lib" / "gamma")
-        mk_pyproject_at(Path.cwd() / "project" / "lib" / "zeta")
-
-        result = runner.invoke(cli, ["list"], catch_exceptions=False)
+    result = runner.invoke(cli, ["list"], catch_exceptions=False)
 
     assert result.exit_code == 0
     assert result.output == dedent(
         """\
-        alpha
-        project/beta
-        project/lib/delta
-        project/lib/gamma
+        //alpha
+        //project/beta
+        //project/lib/delta
+        //project/lib/gamma
         """
     )
 
 
-def test_list__success__respect_gitignore() -> None:
-    find_root.cache_clear()
+def test_list__only_finds_poetry_projects_that_are_not_gitignored(
+    runner: CliRunner,
+) -> None:
+    (Path.cwd() / ".git").mkdir()
+    (Path.cwd() / ".gitignore").write_text(".venv\n")
+    mk_poetry_at(Path.cwd() / "theta")
+    mk_poetry_at(Path.cwd() / ".venv" / "iota")
+    mk_poetry_at(Path.cwd() / "project" / "kappa")
+    mk_poetry_at(Path.cwd() / "project" / ".venv" / "lambda")
 
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        (Path.cwd() / ".git").mkdir()
-        (Path.cwd() / ".gitignore").write_text(".venv\n")
-        mk_poetry_at(Path.cwd() / "theta")
-        mk_poetry_at(Path.cwd() / ".venv" / "iota")
-        mk_poetry_at(Path.cwd() / "project" / "kappa")
-        mk_poetry_at(Path.cwd() / "project" / ".venv" / "lambda")
-
-        result = runner.invoke(cli, ["list"], catch_exceptions=False)
+    result = runner.invoke(cli, ["list"], catch_exceptions=False)
 
     assert result.exit_code == 0
     assert result.output == dedent(
         """\
-        project/kappa
-        theta
+        //project/kappa
+        //theta
         """
     )
 
 
-def test_list__fail__cant_find_root() -> None:
-    find_root.cache_clear()
+def test_list__cant_find_root_when_not_in_git_repo(runner: CliRunner) -> None:
+    mk_poetry_at(Path.cwd() / "alpha")
+    mk_poetry_at(Path.cwd() / "project" / "beta")
+    mk_poetry_at(Path.cwd() / "project" / "lib" / "gamma")
+    mk_poetry_at(Path.cwd() / "project" / "lib" / "delta")
 
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        mk_poetry_at(Path.cwd() / "alpha")
-        mk_poetry_at(Path.cwd() / "project" / "beta")
-        mk_poetry_at(Path.cwd() / "project" / "lib" / "gamma")
-        mk_poetry_at(Path.cwd() / "project" / "lib" / "delta")
-
-        result = runner.invoke(cli, ["list"], catch_exceptions=False)
+    result = runner.invoke(cli, ["list"], catch_exceptions=False)
 
     assert result.exit_code == 1
     assert result.output == dedent(
         """\
         Error: Could not find project root (containing .git directory)
         """
+    )
+
+
+def test_run__runs_command_for_each_poetry_project(runner: CliRunner) -> None:
+    (Path.cwd() / ".git").mkdir()
+    mk_poetry_at(Path.cwd() / "alpha")
+    mk_poetry_at(Path.cwd() / "beta" / "delta")
+
+    result = runner.invoke(cli, ["run", "touch", "new_file"], catch_exceptions=False)
+
+    assert result.exit_code == 0
+
+    assert f"[{style('done', fg='green')}] //alpha" in result.output
+    assert (Path.cwd() / "alpha" / "new_file").is_file()
+
+    assert f"[{style('done', fg='green')}] //beta/delta" in result.output
+    assert (Path.cwd() / "beta" / "delta" / "new_file").is_file()
+
+
+def test_run__prints_output_for_failed_commands(runner: CliRunner) -> None:
+    (Path.cwd() / ".git").mkdir()
+    mk_poetry_at(Path.cwd() / "alpha")
+    (Path.cwd() / "alpha" / "file_only_in_alpha").touch()
+    mk_poetry_at(Path.cwd() / "beta" / "delta")
+
+    result = runner.invoke(
+        cli, ["run", "ls", "file_only_in_alpha"], catch_exceptions=False
+    )
+
+    assert result.exit_code == 0
+
+    assert f"[{style('done', fg='green')}] //alpha" in result.output
+
+    assert f"[{style('failed(2)', fg='red')}] //beta/delta" in result.output
+
+    assert (
+        dedent(
+            """\
+        [//beta/delta] failed with return code 2
+        [//beta/delta:stderr] ls: cannot access 'file_only_in_alpha': No such file or directory
+        """
+        )
+        in result.output
     )
